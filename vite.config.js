2d078a19996232f3bc57cf66ff7e47805cbb2f2d
Joan Zabala <jmjzabala@gmail.com>
2025-12-14 20:29:37 -0400

    feat: Add main index view and update routing
    - Created a new index.blade.php view for the main page layout.
    - Updated the web.php route to return the new index view instead of the welcome view.
    - Modified vite.config.js to use SCSS for styling instead of CSS.

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,19 @@
-import {
-    defineConfig
-} from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    server: {
-        cors: true,
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
+  plugins: [
+    laravel({
+      input: ["resources/sass/app.scss", "resources/js/app.js"],
+      refresh: true,
+    }),
+    tailwindcss(),
+  ],
+  server: {
+    cors: true,
+    watch: {
+      ignored: ["**/storage/framework/views/**"],
     },
+  },
 });
